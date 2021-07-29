d94eafc306e8a2777403ab9a534fda3ec8e6b7f8
SergeyUlyanovAQA <bigbarabumchic@mail.ru>
2021-07-29 22:42:40 +0400

    SelenideTest efectuat

--- src/test/java/selenide/lesson/selenide.java
+++ src/test/java/selenide/lesson/selenide.java
@@ -1,12 +1,29 @@
 package selenide.lesson;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class selenide {
+
+    @BeforeAll
+    static void setup(){
+        Configuration.startMaximized = true;
+    }
 
     @Test
     void selenideTest() {
 
+        open("https://github.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("#js-pjax-container").$(byText("Wikis")).click();
+        $(byText("SoftAssertions")).click();
+        $("#js-repo-pjax-container").shouldHave(text ("How to soft assert using Selenide"));
 
     }
 }
